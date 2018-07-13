72c542332ce696f17473daba5e55f9b26fa5fd87
Gustavo Mendonça Dias <gmdiias@gmail.com>
2018-07-12 22:51:44 -0300

    Desenvolvimento Inicial

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,89 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class Main {
+	
+	HashMap<Integer, Integer> frame = new HashMap<>();
+	List<Pagina> listPaginas = new ArrayList<>(5);
+	
+	int contPF = 0;
+	int modificado = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Simulador de Memória Virtual");
 		
+		Main main = new Main();
+		
+		main.inicializaList();
+		main.lerArquivo();
+		
+		
+		
+	}
+	
+	public void lerArquivo() {
+		BufferedReader br;
+		try {
+			FileReader ler = new FileReader("./entrada.txt");
+			BufferedReader reader = new BufferedReader(ler);
+			String linha;
+			
+			int instrucao;
+			String tipo;
+			int dado;
+
+			while (!(linha = reader.readLine()).equals("#eof")) {
+				linha = linha.replace(":", "");
+				String[] texto = linha.split(" ");
+				instrucao = Integer.parseInt(texto[0].substring(2, 9), 16)/1024;
+				tipo = texto[1];
+				dado = Integer.parseInt(texto[2].substring(2, 9), 16)/1024;
+				
+				verificaHash(instrucao);
+				System.out.println(linha);
+			}
+
+		} catch (IOException e) {
+			System.err.println("Arquivo de dados não encontrado, verifique o caminho ...");
+			e.printStackTrace();
+		}
+	}
+	
+	public void inicializaList() {
+		for(int i = 0; i < 5; i ++) {
+			Pagina nova = new Pagina();
+			listPaginas.add(nova);
+		}
 	}
 
+	public void verificaHash(int posicao) {
+		if(frame.get(posicao) == null) {
+			frame.put(posicao, -1);
+
+		}
+		
+		if(frame.get(posicao) == -1) {
+			algoritmoNRU(posicao);
+		}
+		else {
+			//todo ATUALIZA DADOS ...
+		}
+	}
+	
+	public void algoritmoNRU(int posicao) {
+		for(Pagina pagina : listPaginas) {
+			if(pagina.getNumPag() == -1) {
+				pagina.setNumPag(posicao);
+				pagina.setR(true);
+				pagina.setW(false);
+			}
+		}
+	}
+	
+	
 }
---
+++ src/Pagina.java
@@ -0,0 +1,33 @@
+
+public class Pagina {
+	private int numPag = -1;
+	private boolean r = false;
+	private boolean w = false;
+	private int utilizado = 0;
+	
+	public int getNumPag() {
+		return numPag;
+	}
+	public void setNumPag(int numPag) {
+		this.numPag = numPag;
+	}
+	public boolean isR() {
+		return r;
+	}
+	public void setR(boolean r) {
+		this.r = r;
+	}
+	public boolean isW() {
+		return w;
+	}
+	public void setW(boolean w) {
+		this.w = w;
+	}
+	public int getUtilizado() {
+		return utilizado;
+	}
+	public void setUtilizado(int utilizado) {
+		this.utilizado = utilizado;
+	}
+	
+}
